a11bf30121acb708663fe4fc54a40d108ab1b4ad
bizikoff <bizikovvlad@gmail.com>
2022-08-17 18:15:17 +0300

    worked with pagination (loading new characters on the page)

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,7 @@
 class MarvelService {
   _apiBase = 'https://gateway.marvel.com:443/v1/public/';
   _apiKey = 'apikey=605e9794ec5fa8b7a6ab3a4429002480';
+  _baseOffset = 180;
 
   getRecource = async (url) => {
     let res = await fetch(url);
@@ -12,8 +13,8 @@ class MarvelService {
     return await res.json();
   }
 
-  getAllCharacters = async () => {
-    const res = await this.getRecource(`${this._apiBase}characters?limit=9&offset=180&${this._apiKey}`);
+  getAllCharacters = async (offset = this._baseOffset) => {
+    const res = await this.getRecource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
     return res.data.results.map(this._transformCharacter);
   }
 
